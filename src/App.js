aa1ae06091e84be7126d7cd435f3f01018cbba11
rahulwadate21 <115419561+rahulwadate21@users.noreply.github.com>
2023-05-30 11:25:31 +0530

    impliment app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import DataGrid from './components/DataGrid/DataGrid';
+import Pagination from './components/Pagination/Pagination';
+import Filter from './components/Filter/Filter';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleFilter = (attribute, value) => {
+    // Implement filtering logic
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Data Grid Application</h1>
+      <Filter onFilter={handleFilter} />
+      <DataGrid />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
-}
+};
 
 export default App;
